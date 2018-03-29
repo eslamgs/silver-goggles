cca5cb492773ce44f92531034f89786ede8fa88c
eslamgs <eslamgs5060@gmail.com>
2018-03-29 21:28:30 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter the triangle base and height " << endl;
+    int base , height ;
+    cin>>base>>height;
+    int area = base * height * 0.5 ;
+    cout << "the area is "<<area<<endl;
+    return 0;
+}
